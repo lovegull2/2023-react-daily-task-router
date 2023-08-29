dface7275f94c64bd38c72037447705aa8ac8d17
lovegull <lovegull2@gmail.com>
2023-08-29 20:22:45 +0800

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css';
-import { HashRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { HashRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
+
+function Logout() {
+  const navigate = useNavigate();
+  return (
+    <button onClick= { () => { navigate('/login') } }>
+      Logout
+    </button>
+  )
+}
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+  return (<>
+    <p>這是 Todo 頁面</p>
+    <Logout />
+  </>)
 };
 const Login = () => {
   return <p>這是登入頁面</p>;
